b0e4c303f0d1a2c876c6df787dc1616710c859a7
Henri de Rochefort-Nash <halfire120@gmail.com>
2020-10-29 12:03:19 +0100

    Cleanup and some fixes

--- bento.js
+++ bento.js
@@ -24,6 +24,9 @@ var deleteFolderRecursive = function (source) {
     }
 };
 
+/**
+ * Copies an entire folder
+ */
 var copyFolderRecursive = function (source, destination, ignoreNames) {
     var files = [];
 
@@ -41,19 +44,68 @@ var copyFolderRecursive = function (source, destination, ignoreNames) {
             if (fs.lstatSync(curSource).isDirectory()) {
                 copyFolderRecursive(curSource, destinationFolder);
             } else {
-                console.log("copying: " + path.basename(curSource));
+                console.log("\x1b[33m", "Copying File: " + path.basename(curSource));
                 fs.copyFileSync(curSource, path.join(destinationFolder, file));
             }
         });
     }
 };
 
+/**
+ * Gets confirmation from the user about a question
+ */
+var askConfirmation = function (question, onAccept, onDeny) {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'question',
+        message: question,
+        choices: [
+            'Yes',
+            'No'
+        ]
+    }]).then(function (answers) {
+        if (answers.question === 'Yes') {
+            if (onAccept) {
+                onAccept();
+            }
+        } else if (answers.question === 'No') {
+            if (onDeny) {
+                onDeny();
+            }
+        }
+    });
+};
+
+/**
+ * Executes a general command with logs
+ * @param {String} cmd - The command
+ * @param {Array} args - an array with arguments
+ * @param {Function} [onComplete] - execute after completion
+ */
+var execCommand = function (cmd, args, onComplete) {
+    var spawn = require('child_process').spawn;
+    var task = spawn(cmd, args, {
+        stdio: "inherit"
+    });
+    task.on('exit', function (code) {
+        console.log("\x1b[31m", 'Exited with code ' + code.toString());
+        if (onComplete) {
+            onComplete();
+        }
+    });
+};
+
+
+/**
+ * Entry Point
+ */
 var main = function () {
+    console.log("\x1b[36m", "\nBento Command Line Interface 🤖 \n");
     var prompt = function () {
         var strings = {
-            newProject: "New 2D Bento project...",
-            newProject3D: "New 3D Bento project...",
-            openDoc: "Open documentation"
+            newProject: "🕹  New 2D Bento project...",
+            newProject3D: "🍙 New 3D Bento project...",
+            openDoc: "📖 Open documentation"
         };
 
         inquirer.prompt([{
@@ -93,50 +145,31 @@ var main = function () {
         prompt();
     }
 };
-var askConfirmation = function (question, onAccept, onDeny) {
-    inquirer.prompt([{
-        type: 'list',
-        name: 'question',
-        message: question,
-        choices: [
-            'Yes',
-            'No'
-        ]
-    }]).then(function (answers) {
-        if (answers.question === 'Yes') {
-            if (onAccept) {
-                onAccept();
-            }
-        } else if (answers.question === 'No') {
-            if (onDeny) {
-                onDeny();
-            }
-        }
-    });
-};
+
+
 /**
- * Executes a general command with logs
- * @param {String} cmd - The command
- * @param {Array} args - an array with arguments
- * @param {Function} [onComplete] - execute after completion
- */
-var execCommand = function (cmd, args, onComplete) {
-    var spawn = require('child_process').spawn;
-    var task = spawn(cmd, args, {
-        stdio: "inherit"
-    });
-    task.on('exit', function (code) {
-        console.log('Exited with code ' + code.toString());
-        if (onComplete) {
-            onComplete();
-        }
-    });
-};
-/**
- * Download bento empty project
+ * Downloads bento empty project, and configures it
  */
 var newBentoProject = function (branch) {
     var projectName = "";
+
+    var askProjectFolder = function () {
+        // ask for project name
+        inquirer.prompt([{
+            type: 'input',
+            name: 'projectName',
+            message: "Name your project/repo (no spaces):"
+        }]).then(function (answers) {
+            projectName = answers.projectName || "EmptyBentoProject";
+
+            if (projectName.indexOf(' ') >= 0) {
+                askProjectFolder();
+                return;
+            }
+            downloadEmptyProject(projectName);
+        });
+    };
+
     var downloadEmptyProject = function (projectName) {
         var request = require('request');
         var AdmZip = require('adm-zip');
@@ -144,15 +177,17 @@ var newBentoProject = function (branch) {
         var cwd = process.cwd();
         projectPath = path.join(cwd, projectName);
         var url = 'https://github.com/LuckyKat/Bento-Empty-Project/archive/' + branch + '.zip';
+
+
         var download = function () {
             var tmpFilePath = path.join(cwd, "temp.zip");
-            console.log("Downloading bento project...");
+            console.log("\x1b[33m", "Downloading bento project...");
             request(url)
                 .pipe(fs.createWriteStream(tmpFilePath))
                 .on('close', function () {
                     var zip = new AdmZip(tmpFilePath);
                     zip.extractAllTo(cwd);
-                    console.log("Unarchiving...");
+                    console.log("\x1b[33m", "Unarchiving...");
 
                     // clean up temp zip
                     fs.unlink(tmpFilePath, function () {});
@@ -162,13 +197,14 @@ var newBentoProject = function (branch) {
                     afterInstall();
                 });
         };
+
         var install = function () {
             var commandExists = require('command-exists');
 
             // enter new directory
             process.chdir(projectPath);
             // call npm install or yarn
-            console.log("Installing development tools...");
+            console.log("\x1b[33m", "Installing development tools...");
             commandExists('yarn', function (err, commandExists) {
                 if (commandExists) {
                     execCommand('yarn', [], afterInstall);
@@ -181,63 +217,50 @@ var newBentoProject = function (branch) {
 
 
         var afterInstall = function () {
+            var cwd = process.cwd();
+
             // clean up
-            console.log("Cleaning up...");
+            console.log("\x1b[33m", "Cleaning up...");
             if (fs.existsSync('readme.md')) {
                 fs.unlinkSync('readme.md');
             }
             if (fs.existsSync('changelog.md')) {
                 fs.unlinkSync('changelog.md');
             }
-            // TODO: optional - clean up package.json with real project name and author
-            // could also ask about other game settings
-            console.log("Installation complete!");
-            console.log("Use the `bento` command in a project folder to view development tasks.");
 
+            var packageJsonPath = path.join(projectPath, 'package.json');
             var packageJsonStr;
             var packageJson;
+            var gameJsPath = path.join(projectPath, 'js', 'game.js');
             var gameJsStr;
-            var configStr;
-            var indexStr;
-            if (fs.existsSync('package.json')) {
-                packageJsonStr = fs.readFileSync(path.join('package.json'), 'utf-8');
+            if (fs.existsSync(packageJsonPath)) {
+                packageJsonStr = fs.readFileSync(packageJsonPath, 'utf-8');
                 packageJson = JSON.parse(packageJsonStr);
 
                 packageJson.name = projectName;
 
-                fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 4));
+                console.log("\x1b[33m", "Setting project name in package.json...");
+
+                fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 4));
             }
 
-            if (fs.existsSync(path.join('js', 'game.js'))) {
-                gameJsStr = fs.readFileSync(path.join('js', 'game.js'), 'utf-8');
+            if (fs.existsSync(gameJsPath)) {
+                gameJsStr = fs.readFileSync(gameJsPath, 'utf-8');
 
                 gameJsStr = gameJsStr.replace("Bento.saveState.setId('EmptyProject/');", "Bento.saveState.setId('" + projectName + "/');");
                 gameJsStr = gameJsStr.replace("name: 'Empty Project',", "name: '" + projectName + "',");
 
-                fs.writeFileSync(path.join('js', 'game.js'), gameJsStr);
-            }
 
-            if (fs.existsSync(path.join('config.xml'))) {
-                configStr = fs.readFileSync(path.join('config.xml'), 'utf-8');
+                console.log("\x1b[33m", "Updating game.js with project name ...");
 
-                configStr = configStr.replace('<name>Bento Empty Project</name>', '<name>' + projectName + '</name>');
-
-                fs.writeFileSync(path.join('config.xml'), configStr);
-            }
-
-            if (fs.existsSync(path.join('index.html'))) {
-                indexStr = fs.readFileSync(path.join('index.html'), 'utf-8');
-
-                indexStr = indexStr.replace('<title>Empty Project</title>', '<title>' + projectName + '</title>');
-
-                fs.writeFileSync(path.join('index.html'), indexStr);
+                fs.writeFileSync(gameJsPath, gameJsStr);
             }
 
             // remove git history
             deleteFolderRecursive(path.join('.git'));
 
-
-            checkForPluginsFolder();
+            // further steps
+            askDisplayName();
         };
         if (fs.existsSync(projectPath)) {
             console.error('The folder ' + projectName + ' already exists!');
@@ -246,6 +269,100 @@ var newBentoProject = function (branch) {
 
         download();
     };
+
+
+    var askDisplayName = function () {
+        var cwd = process.cwd();
+
+        // preemptively read out config.xml
+        var configXMLPath = path.join(projectPath, 'config.xml');
+        var indexHTMLPath = path.join(projectPath, 'config.xml');
+
+        // ask for display name
+        inquirer.prompt([{
+            type: 'input',
+            name: 'displayName',
+            message: "What display name would you like to use?:"
+        }]).then(function (answers) {
+            var displayName = answers.displayName || "Empty Bento Project";
+
+            if (fs.existsSync(configXMLPath)) {
+                var configXMLStr = fs.readFileSync(configXMLPath, 'utf-8');
+                // replace
+                configXMLStr = configXMLStr.replace('<name>Bento Empty Project</name>', '<name>' + displayName + '</name>');
+
+                console.log("\x1b[33m", "Updating config.xml with display name ...");
+
+                //write back out the config.xml
+                fs.writeFileSync(configXMLPath, configXMLStr);
+            }
+
+            if (fs.existsSync(indexHTMLPath)) {
+                var indexHTMLStr = fs.readFileSync(indexHTMLPath, 'utf-8');
+                // replace
+                indexHTMLStr = indexHTMLStr.replace('<title>Empty Project</title>', '<title>' + displayName + '</title>');
+                indexHTMLStr = indexHTMLStr.replace('<title>Onigiri</title>', '<title>' + displayName + '</title>');
+
+                console.log("\x1b[33m", "Updating index.html with display name ...");
+
+                //write back out the config.xml
+                fs.writeFileSync(indexHTMLPath, indexHTMLStr);
+            }
+
+            askBundleId();
+        });
+
+    };
+
+
+    var askBundleId = function () {
+        var cwd = process.cwd();
+
+        // preemptively read out config.xml
+        var configXMLPath = path.join(projectPath, 'config.xml');
+        // ask for bundleId
+        inquirer.prompt([{
+            type: 'input',
+            name: 'bundleId',
+            message: "What Bundle ID would you like to use (e.g. 'com.company.projectname')?:"
+        }]).then(function (answers) {
+            var bundleId = answers.bundleId || "com.luckykat.app";
+
+            if (fs.existsSync(configXMLPath)) {
+                var configXMLStr = fs.readFileSync(configXMLPath, 'utf-8');
+                configXMLStr = configXMLStr.replace('id="com.luckykat.app"', 'id="' + bundleId + '"');
+
+                console.log("\x1b[33m", "Updating config.xml with bundle ID...");
+
+                //write back out the config.xml
+                fs.writeFileSync(configXMLPath, configXMLStr);
+            }
+
+            checkForPluginsFolder();
+        });
+    };
+
+    var checkForPluginsFolder = function () {
+        var moveOn = function () {
+            console.log("\x1b[32m", '\n\nYou\'re all set! 👍');
+            console.log("\x1b[37m", "\nUse the `bento` command in a project folder to view development tasks.");
+        };
+        var cwd = process.cwd();
+        if (fs.existsSync(path.join(cwd, 'cordova-plugins/')) && fs.existsSync(path.join(cwd, 'bento-modules/'))) {
+            askConfirmation('cordova-plugins and bento-modules detected, would you like to install the default plugins and modules?', function () {
+                addPlugins('default');
+                addModulesFromList('default');
+                moveOn();
+            }, function () {
+                console.log("okay...");
+                moveOn();
+            });
+        } else {
+            console.log('cordova-plugins or bento-modules not detected.');
+            moveOn();
+        }
+    };
+
     var addPlugins = function (snippet) {
         var cwd = process.cwd();
 
@@ -256,17 +373,19 @@ var newBentoProject = function (branch) {
         //get the path to the plugin folder
         var cordovaPluginsPath = path.join(cwd, 'cordova-plugins/');
         var pluginsSnippetStr;
-
+        console.log("\x1b[33m", "\n Plugins: ");
         //replace stuff in the config xml with our found snippet
         if (fs.existsSync(path.join(cordovaPluginsPath, snippet))) {
             pluginsSnippetStr = fs.readFileSync(path.join(cordovaPluginsPath, snippet), 'utf-8');
             var pluginsLineStart = "<!-- Include plugins here -->";
             configXMLStr = configXMLStr.replace(pluginsLineStart, pluginsLineStart + '\n' + pluginsSnippetStr);
         }
+        console.log("\x1b[33m", "Adding plugin snippet to config.xml ...");
 
         //write back out the config.xml
         fs.writeFileSync(configXMLPath, configXMLStr);
     };
+
     var addModulesFromList = function (list) {
         var cwd = process.cwd();
 
@@ -283,8 +402,13 @@ var newBentoProject = function (branch) {
             moduleListStr = fs.readFileSync(path.join(bentoModulesPath, list + '.json'), 'utf-8');
             moduleList = JSON.parse(moduleListStr);
 
+            console.log("\x1b[33m", "\n Modules: ");
+
             // for each module on the list we just found
             moduleList.forEach(function (moduleFolder) {
+
+                console.log("\x1b[33m", "\n Adding Module: " + moduleFolder);
+
                 var modulePath = path.join(bentoModulesPath, moduleFolder);
                 //copy everything over
                 var jsPath = path.join(modulePath, 'js');
@@ -326,6 +450,7 @@ var newBentoProject = function (branch) {
                 var functionLineStart = "var initFunctions = {";
                 var fullSnippet = "";
                 if (initDataObject && initDataObject.name && initSnippetStr) {
+                    console.log("\x1b[33m", "Adding Init Snippet to init.js ...");
                     fullSnippet = "\n" + initDataObject.name + ":" + initSnippetStr + ",";
                     initJSStr = initJSStr.replace(functionLineStart, functionLineStart + fullSnippet);
                 }
@@ -335,45 +460,10 @@ var newBentoProject = function (branch) {
         //write back out the init.js
         fs.writeFileSync(initJSPath, initJSStr);
     };
-    var checkForPluginsFolder = function () {
-        var moveOn = function () {
-            console.log('\n\nYou\'re all set!');
-        };
-        var cwd = process.cwd();
-        if (fs.existsSync(path.join(cwd, 'cordova-plugins/')) && fs.existsSync(path.join(cwd, 'bento-modules/'))) {
-            askConfirmation('cordova-plugins and bento-modules detected, would you like to install the default plugins and modules?', function () {
-                addPlugins('default');
-                addModulesFromList('default');
-                moveOn();
-            }, function () {
-                console.log("okay...");
-                moveOn();
-            });
-        } else {
-            console.log('cordova-plugins not detected.');
-            moveOn();
-        }
-    };
-    var askProjectName = function () {
-        // ask for project name
-        inquirer.prompt([{
-            type: 'input',
-            name: 'projectName',
-            message: "Name your new project (no spaces):"
-        }]).then(function (answers) {
-            projectName = answers.projectName || "EmptyBentoProject";
-
-            if (projectName.indexOf(' ') >= 0) {
-                askProjectName();
-                return;
-            }
-            downloadEmptyProject(projectName);
-        });
-    };
 
     branch = branch || 'master';
 
-    askProjectName();
+    askProjectFolder();
 };
 
 main();
